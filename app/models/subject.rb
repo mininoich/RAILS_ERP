85e554b076f391263e45df56ea8ee82d274e683c
mininoich <mininoich@gmail.com>
2012-04-21 11:39:41 +0200

    On delete cascade sur subjects

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,5 @@
 class Subject < ActiveRecord::Base
   attr_accessible :name
-  has_many :abilities
+  has_many :abilities, :dependent => :delete_all #ON DELETE CASCADE
   has_many :teachers, :through => :abilities
 end
