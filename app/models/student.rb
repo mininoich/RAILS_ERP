582e89742e2655da41f086a7a147d9689cf08bb5
mininoich <mininoich@gmail.com>
2012-04-22 16:33:20 +0200

    conflit

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,9 +2,5 @@ class Student < ActiveRecord::Base
   attr_accessor :name, :email, :password
   attr_accessible :name, :email, :password
   belongs_to :user
-<<<<<<< HEAD
-end
-=======
   belongs_to :klass
 end
->>>>>>> students_klasses
