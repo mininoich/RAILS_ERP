bf2e7ed7ccb14e5604d2f22a0aa6800560e6b800
jaouad <jbenasaid@gmail.com>
2012-04-29 17:23:00 +0200

    ajout fichier group2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120421132703) do
+ActiveRecord::Schema.define(:version => 20120422213302) do
 
   create_table "abilities", :force => true do |t|
     t.integer  "teacher_id"
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(:version => 20120421132703) do
   end
 
   add_index "admins", ["user_id"], :name => "index_admins_on_user_id"
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "students", :force => true do |t|
     t.integer  "user_id"
